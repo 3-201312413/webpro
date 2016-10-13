c3fc6aa8ee7804f6ad2fc51e4b0c9405a016ed40
705 <705@705-26-PC>
2016-10-13 13:55:03 +0900

    첫 소스 버전 올림

---
+++ 3_201312413_ex00/src/test/java/org/zerock/web3/MySQLConnectionTest.java
@@ -0,0 +1,34 @@
+package org.zerock.web3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySQLConnectionTest {
+
+	private static final String DRIVER = 
+			"com.mysql.jdbc.Driver";
+	private static final String URL = 
+			"jdbc:mysql://127.0.0.1:3306/3_201312413_book_ex";
+	private static final String USER = 
+			"201312413";
+	private static final String PW = 
+			"201312413";
+			
+	
+	@Test
+	public void testConnection() throws Exception{
+		
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			
+			System.out.println(con);
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
